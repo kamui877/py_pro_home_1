8c9a691d2884c8bc5d71e686a0e6d176263326d1
Dmitriy Krivchikov <dima.dima2001.dima@mail.ru>
2022-07-01 13:24:08 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from application.salary import calculate_salary
+from application.db.people import get_employees
+import datetime
+
+if __name__ == '__main__':
+    print(datetime.date.today())
+    calculate_salary()
+    get_employees()
